ffb38158ab8335901ce4c7062b1a3cb4b161c211
itgsod-jonatan-lovqvist <jonatan.lovqvist@elev.it-gymnasiet.se>
2015-03-24 09:59:48 +0100

    uppgift 1c

--- dices/uppgift17a.rb
+++ dices/uppgift17a.rb
@@ -21,7 +21,7 @@ diff3 = 0
 
 (1..6).each do |dice1|
   (1..6).each do |dice2|
-    if (dice1-dice2).abs == 4
+    if (dice1+dice2).abs == 7
       diff3 += 1
       print "#{dice1},#{dice2} ".red
     else
